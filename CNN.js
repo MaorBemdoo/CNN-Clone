7eeb72c74e3fe5e6a15fb76baa7f05180ceed586
Bemdoo Maor <bemdoo.maor@gmail.com>
2023-06-24 21:02:13 +0100

    Finished working on the first article of #news2

--- CNN.js
+++ CNN.js
@@ -1,8 +1,8 @@
 
 $("document").ready(function(){
     window.onscroll = () => {
-        $("#Hdiv").css("margin-top", scrollY + "px").css("z-index", "100")
-        if(scrollY >= 296){
+        $("#Hdiv").css("margin-top", scrollY+70 + "px").css("z-index", "100")
+        if(scrollY >= 296-70){
             $("#Hdiv").css("z-index", "-100")
         }
     }
