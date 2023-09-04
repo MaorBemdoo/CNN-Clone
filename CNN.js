f4a2b324a14ac95c119a4b173ed248560f818e0e
Bemdoo Maor <bemdoo.maor@gmail.com>
2023-09-04 09:39:28 +0100

    Implemented navDivFisrtChild in window.onresive

--- CNN.js
+++ CNN.js
@@ -18,14 +18,22 @@ $("document").ready(function () {
 		$("footer article:nth-child(3) form button").html(`<i class="bi bi-search" style="font-size: 1.2rem;"></i>`)
 	}
 
-	const navDivFirstChild = document.querySelectorAll("nav div:first-child");
-	// window.onresize = (e) => {
-	// 	console.log(e.target.screen.width);
-	// 	if (e.target.screen.width <= 1153) {
-	// 		navDivFirstChild.forEach((child) => {
-	// 			child.lastElementChild.style.display = "block";
-	// 			child.lastElementChild.previousElementSibling.style.display = "none";
-	// 		});
-	// 	}
-	// };
+	const navDivFirstChild = $("nav div:first-child")
+	// if(window.innerWidth <= 1207){
+	// 	navDivFirstChild.children().last().css("display", "block")
+	// 	navDivFirstChild.children().last().prev().css("display", "none")
+	// }
+	window.onresize = (e) => {
+		console.log(e.target.screen.width);
+		if (e.target.screen.width <= 1207) {
+			navDivFirstChild.children().last().css("display", "block")
+			navDivFirstChild.children().last().prev().css("display", "none")
+		}
+		if(e.target.screen.width <= 1111){
+			navDivFirstChild.children().last().prev().prev().css("display", "none")
+		}
+		if(e.target.screen.width <= 1047){
+			navDivFirstChild.children().last().prev().prev().prev().css("display", "none")
+		}
+	};
 });
